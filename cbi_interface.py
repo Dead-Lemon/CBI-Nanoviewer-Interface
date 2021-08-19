0d7057f2008359aa4b32046bcb32542339578cbb
dead-lemon <wslatem@gmail.com>
2021-08-20 00:21:37 +0200

    some serial processing

--- cbi_interface.py
+++ cbi_interface.py
@@ -30,9 +30,10 @@ Protocal Spec
 """
 import struct
 import time
+import serial
 
 test1 = b'\xE6\x00'
-serialDataBuffer1 = b'\x11\xE6\x00\xE0\x2E\x64\x00\xC8\x00\x2C\x01\x90\x01\xF4\x01\x58\x02\xBC\x02\x20\x03\x84\x03\xE8\x03\x4C\x04\xB0\x04\x14\x05\x78\x05\xDC\x05\xFB\xD5'
+serialDataBuffer1 = b'\xAA\x10\xE6\x00\xE0\x2E\x64\x00\xC8\x00\x2C\x01\x90\x01\xF4\x01\x58\x02\xBC\x02\x20\x03\x84\x03\xE8\x03\x4C\x04\xB0\x04\x14\x05\x78\x05\xDC\x05\xFB\xD5'
 serialDataBuffer2 = b'\xAA\x11\xC0\xD4\x01\x00\0xE8\x03\x00\x00\xD0\x07\x00\x00\0xB8\x0B\x00\x00\0xA0\x0F\x00\x00\x88\x13\x00\x00\x70\x17\x00\x00\x58\x1B\x00\x00\x40\x1F\x00\x00\x28\x23\x00\x00\0x10\x27\x00\x00\0xF8\x2A\x00\x00\0xE0\x2E\x00\x00\xC8\x32\x00\x00\xB0\x36\x00\x00\x98\x3A\x00\x00\x21\x80'
 serialDataBuffer3 = b'\xAA\x12\xFF\x00\x00'
 #data sample to test with
@@ -62,26 +63,54 @@ def testCRC(data):
     else:
         return False
 
+def checkID(data):
+    switch={
+    b'\x10':livePowerBuffSize,
+    b'\x11':accEnergyBuffSize,
+    b'\x12':firmwareBuffSize
+    }
+    return switch.get(data, 'null')
 
-print(testCRC(serialDataBuffer1[1:]))
+def processLivePower(data):
+    newData = struct.unpack('<' + 'H', data)
+    print(newData)
+
+def processAccEnergy(data):
+
+def processFirmware(data):
 
 
-
-
-
-
-"""
-# Start of Execution
 try:
-    ser = serial.Serial('/dev/ttyS0', baudrate=2400, timeout=1)  # open first serial port
+    cbiSerial = serial.Serial('/dev/ttyUSB0', baudrate=2400, timeout=0.5)  # open first serial port
+    cbiSerial.write(serialDataBuffer1)
 except:
     print("COM PORT NOT OPEN!!!")
+    quit()
 
 
 buffer = bytearray(0)
 
 while True:
-    buffer += ser.read(1)
-    while decode_string(buffer):
-        pass
-"""
+    if (cbiSerial.inWaiting()): #or rather read amoutn waiting and check through the list for a start ID, over just flushing the buffer
+        if (cbiSerial.read()==packetStartID):
+            ID = cbiSerial.read()
+            print(ID)
+            packetSize = checkID(ID)
+            if (packetSize == 'null'):
+                print("invalid packet ID")
+                cbiSerial.flush()
+                pass
+            print(packetSize)
+            buffer = cbiSerial.read(packetSize+2)
+            print(buffer)
+            print(testCRC(buffer))
+            if (testCRC(buffer)):
+
+            else:
+                print("CRC check failed")
+                cbiSerial.flush()
+
+            quit()
+    else:
+        cbiSerial.flush()
+        time.sleep(1)
